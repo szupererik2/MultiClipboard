9c4594e140a6f560f08a8704888dc76da4c2a4e3
szupererik2 <szegedierik04@gmail.com>
2025-10-05 00:19:46 +0300

    Paste added

---
+++ src/clip_manage/paste.py
@@ -0,0 +1,9 @@
+import pyperclip
+import clipboard
+import keyboard
+
+def paste_from_clip(c, key):
+   
+   paste_data = c.req_data(key)
+   pyperclip.copy(paste_data)
+   keyboard.press_and_release('ctrl+v')
