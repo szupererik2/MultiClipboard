4ede39f2491ff618e4dcddb7920c0dd1d82cafd7
szupererik2 <szegedierik04@gmail.com>
2025-10-05 10:21:26 +0300

    Insted of a while looping around added keyboard listeners and changed the base logic

--- src/Key_press.py
+++ src/Key_press.py
@@ -4,15 +4,9 @@ from clip_manage.copy import copy_from_clip
 from clip_manage.paste import paste_from_clip
 c = clip()
 
-while True:
-    try:
-        if keyboard.is_pressed('ctrl') and keyboard.is_pressed('c'):
-            for i in range(1,13):
-                if(keyboard.is_pressed(f'f{i}')):
-                    copy_from_clip(c, i)
-        elif keyboard.is_pressed('ctrl') and keyboard.is_pressed('v'):
-            for i in range(1,13):
-                if(keyboard.is_pressed(f'f{i}')):
-                    paste_from_clip(c, i)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+for i in range(1, 13):
+    keyboard.add_hotkey(f"ctrl+f{i}", lambda i=i: copy_from_clip(c, i))
+    keyboard.add_hotkey(f"shift+f{i}", lambda i=i: paste_from_clip(c, i))
+
+print("Press ESC to quit")
+keyboard.wait("esc")
